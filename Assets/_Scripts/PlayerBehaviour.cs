6a095b8518e950421e12ada3b6dfef5db3a2d8fc
Simon <scdg98@outlook.com>
2021-02-06 17:55:26 -0500

    added CameraController

--- Assets/_Scripts/PlayerBehaviour.cs
+++ Assets/_Scripts/PlayerBehaviour.cs
@@ -5,30 +5,78 @@ using UnityEngine;
 public class PlayerBehaviour : MonoBehaviour
 {
     public float movementForce;
-    public Rigidbody rigidbody;
+    public float jumpForce;
+    public Rigidbody rigidBody;
+    public bool isGrounded;
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody>();
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxisRaw("Horizontal") > 0)
+        if (isGrounded)
         {
-            // move to the right
+            if (Input.GetAxisRaw("Horizontal") > 0)
+            {
+                // move to the right
 
-            //two ways to typen the movement to the right times movementforce
-            //rigidbody.AddForces(new Vector3(1.0f, 0.0f, 0.0f) * movementForce);
-            rigidbody.AddForce(Vector3.right * movementForce);
+                //two ways to typen the movement to the right times movementforce
+                //rigidbody.AddForces(new Vector3(1.0f, 0.0f, 0.0f) * movementForce);
+                rigidBody.AddForce(Vector3.right * movementForce);
+            }
+
+            if (Input.GetAxisRaw("Horizontal") < 0)
+            {
+                // move to the left
+                rigidBody.AddForce(Vector3.left * movementForce);
+            }
+
+            if (Input.GetAxisRaw("Vertical") > 0)
+            {
+                // move forward
+                rigidBody.AddForce(Vector3.forward * movementForce);
+            }
+
+            if (Input.GetAxisRaw("Vertical") < 0)
+            {
+                // move to the back
+                rigidBody.AddForce(Vector3.back * movementForce);
+            }
+
+            if (Input.GetAxisRaw("Jump") > 0)
+            {
+                //the object will jump
+                rigidBody.AddForce(Vector3.up * jumpForce);
+            }
         }
-
-        if (Input.GetAxisRaw("Horizontal") < 0)
-        {
-            // move to the left
-            rigidbody.AddForce(Vector3.left * movementForce);
-        }
-
     }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+        
+    }
+
+    void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
 }
